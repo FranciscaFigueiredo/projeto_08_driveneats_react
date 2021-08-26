c5fb6c458f6a7e657cbbe0d1e20bac2fce8b7931
Francisca <fraan928@gmail.com>
2021-08-26 03:20:17 -0300

    Checking and unchecking selected foods

--- src/shared/Cardapio.js
+++ src/shared/Cardapio.js
@@ -1,20 +1,41 @@
+import React, { useState } from "react";
+
 export default function Cardapio(props) {
+    const [classe, setClasse] = useState("escolher-pedido");
+    const [quantidade, setQuantidade] = useState("setar-quantidade hidden");
+    const [quant, setQuant] = useState(1);
+
+    function selecionarPedido() {
+        if(classe === "escolher-pedido") {
+            setClasse("escolher-pedido prato-selecionado");
+            setQuantidade("setar-quantidade");
+        } else {
+            setClasse("escolher-pedido");
+            setQuantidade("setar-quantidade hidden");
+        }
+    }
+
     const {
         srcImagem,
         nome,
         descricao,
         valor
     } = props;
+
     
     return (
-        <div class="escolher-pedido" onclick="selecionarPrato(this);">
-            <img src={srcImagem} alt="" />
-            <h4><strong>{nome}</strong></h4>
-            <h5>{descricao}</h5>
-            <p>R$ <span class="valor-prato">{valor}</span></p>
-            <div class="icone">
-                <ion-icon name='checkmark-circle'></ion-icon>
-            </div>                
+        <div className={classe}>
+            <div className="click" onClick={selecionarPedido}>
+                <img src={srcImagem} alt="" />
+                <h4><strong>{nome}</strong></h4>
+                <h5>{descricao}</h5>
+            </div>
+            <div className="rodape-alimento">
+                <p><span>R$ <span className="valor-prato">{valor}</span></span></p>
+                <div className={quantidade}>
+                    <span className="diminuir-quantidade" onClick={() => (quant > 1 ? (setQuant(quant - 1)) : (setClasse("escolher-pedido"), setQuantidade("setar-quantidade hidden")))}>-</span> <span>{quant}</span> <span onClick={() => setQuant(quant + 1)}><strong> +</strong></span>
+                </div>
+            </div>              
         </div>
     )
 }
